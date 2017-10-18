7d76827e030b404e1cc6bf0a3a0cd4fc1044ee68
squidism <rbarbera@udel.edu>
2017-10-18 11:58:13 -0400

    Supers
    unit package made
    Renamed enemy package to fit naming conventions, testing to see if it breaks anything

---
+++ Estuary Turret Defense/src/unit/UnitModel.java
@@ -0,0 +1,58 @@
+package unit;
+
+import java.awt.image.BufferedImage;
+
+public abstract class UnitModel {
+	String Name;
+	String Id;
+	int xCor;
+	int yCor;
+	BufferedImage[] Icon;
+	String GameDescription;
+	String FactDescription;
+	
+	
+	public String getName() {
+		return Name;
+	}
+	public String getId() {
+		return Id;
+	}
+	public void setId(String id) {
+		Id = id;
+	}
+	public void setName(String name) {
+		Name = name;
+	}
+	
+	public int getxCor() {
+		return xCor;
+	}
+	public void setxCor(int xCor) {
+		this.xCor = xCor;
+	}
+	public int getyCor() {
+		return yCor;
+	}
+	public void setyCor(int yCor) {
+		this.yCor = yCor;
+	}
+	public BufferedImage[] getIcon() {
+		return Icon;
+	}
+	public void setIcon(BufferedImage[] icon) {
+		Icon = icon;
+	}
+	public String getGameDescription() {
+		return GameDescription;
+	}
+	public void setGameDescription(String gameDescription) {
+		GameDescription = gameDescription;
+	}
+	public String getFactDescription() {
+		return FactDescription;
+	}
+	public void setFactDescription(String factDescription) {
+		FactDescription = factDescription;
+	}
+}
