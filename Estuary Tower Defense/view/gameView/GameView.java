d81e5aabf162c0fdfa706527af6d50d5f48f8f57
davidtp2018 <davidtp@udel.edu>
2017-11-27 22:48:37 -0500

    Documentation

--- Estuary Tower Defense/view/gameView/GameView.java
+++ Estuary Tower Defense/view/gameView/GameView.java
@@ -5,10 +5,17 @@ import leaderBoardView.LeaderBoardView;
 import mainMenuView.MainMenuView;
 
 /**
- * Creates the gameview to create instances of the MainMenuView and BoardMenuView.
+ * Creates the gameview to create instances of the MainMenuView, BoardMenuView
+ * and leadershipViews .
  * 
  * @author Ryan Barbera, Aaron George, Nick Norton, Thomas Pennington, Grant Zhao
  *
+ */
+
+/*
+ * MainMenuView mainMenu: an instance of the main menu view
+ * BoardMenuView boardMenu: an instance of the board menu view
+ * LeaderBoardView leaderBoard: an instance of the leader board view 
  */
 
 public class GameView {
@@ -17,7 +24,7 @@ public class GameView {
 	public LeaderBoardView leaderBoard;
 	
 	/**
-	 * Sets up the instances for the main menu and the board menu views. 
+	 * Sets up the instances for the main menu, board menu, and leadership views. 
 	 */
 	public GameView() {
 		mainMenu = new MainMenuView();
